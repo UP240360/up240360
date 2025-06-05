c77dfe7529836aadca91d8c2e4cb1f0a8082c18b
UP240360 <up240360@alumnos.upa.edu.mx>
2025-06-05 12:52:14 -0600

    inicio del control de versiones

---
+++ day_1/prueba.py
@@ -0,0 +1,23 @@
+# Dia 1
+
+print (3+4)
+print (3-4)
+print (3%4)
+print (3*4)
+print (3**4)
+print (3//4)
+print ("Daniel Isaac")
+print ("Nava Perez")
+print ("Mexico")
+print ("I am enjoying 30 days of python")
+print (type(10))
+print (type(9.8))
+print (type(4 - 4j))
+print (type(["Asabeneh", "Python", "Finland"]))
+print (type("Daniel Isaac"))
+print (type("Nava Perez"))
+print (type("Mexico"))
+
+# Dia 2
+
+
---
+++ day_2/variables.py
@@ -0,0 +1,32 @@
+#Day 2: 30 Days of python programming
+
+nombre = "Dany"
+apellido = "Nava"
+nombre_completo = nombre + "" + apellido
+pais = "Mexico"
+ciudad = "Aguascalientes"
+edad = 18
+año = 2006
+casado = "no"
+is_true = "true"
+is_light_on = "si"
+
+nombre, apellido, nombre_completo, pais, ciudad, edad, año, casado, is_true, is_light_on
+
+print(type(nombre))
+print(type(apellido))
+print(type(nombre_completo))
+print(type(pais))
+print(type(ciudad))
+print(type(edad))
+print(type(año))
+print(type(casado))
+print(type(is_true))
+print(type(is_light_on))
+
+#len
+print(len(nombre))
+
+#comparar
+print(len(nombre)) > len(apellido)
+print(len(nombre)) == len(apellido)
